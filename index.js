6adde58daac366ddd1ec11239879ff4d1fb6b2f6
Timofei1 <57501337+Timofei1@users.noreply.github.com>
2023-02-12 08:21:55 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,46 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require("express");
+const fs = require("fs");
+const cors = require("cors");
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+
+app.get("/api/chat-history", (req, res) => {
+    fs.readFile("./chat-history.json", "utf8", (err, data) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.send(JSON.parse(data));
+        }
+    });
+});
+
+app.post("/api/chat-history", (req, res) => {
+    fs.readFile("./chat-history.json", "utf8", (err, data) => {
+        if (err) {
+            const initialData = [];
+            fs.writeFile("./chat-history.json", JSON.stringify(initialData), (err) => {
+                if (err) {
+                    res.status(500).send(err);
+                }
+            });
+            data = JSON.stringify(initialData);
+        }
+
+        let chatHistory = JSON.parse(data);
+        chatHistory.push(req.body);
+        fs.writeFile("./chat-history.json", JSON.stringify(chatHistory), (err) => {
+            if (err) {
+                res.status(500).send(err);
+            } else {
+                res.send({ message: "Message added successfully!" });
+            }
+        });
+    });
+});
+
+app.listen(5000, () => {
+    console.log("Server started on port 5000");
+});
